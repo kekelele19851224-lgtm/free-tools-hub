fdc785faf5d789d2df8f3b41f0a2e36a83be1689
kekelele19851224-lgtm <kekelele19851224@gmail.com>
2026-01-03 16:31:25 +0800

    Fix homepage card equal height

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
           </div>
 
           {/* Tool Grid */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 auto-rows-fr">
             {filteredCalculators.map((calc) => (
               <CalculatorCard
                 key={calc.url}
